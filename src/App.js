ad7a5035928fe1471cc26f92fc32779fb07f2272
syed_saqib <syedsaqib.work@gmail.com>
2024-03-04 20:32:10 +0530

    added UI components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Dashboard from './Components/Dashboard/Dashboard';
+import Reports from './Components/Reports/Reports';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route exact path="/home" element={<Dashboard />} />
+          <Route exact path="/reports" element={<Reports />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
